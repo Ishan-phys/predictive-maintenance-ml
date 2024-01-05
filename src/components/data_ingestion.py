2035cb32646dd2d90ff65173ff6fbaf85e970a27
Ishan-phys <ishan.alld@gmail.com>
2024-01-05 12:04:59 +0530

    setup.py file setup

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,9 @@
+from src.logger import logger
+
+
+
+if __name__ == '__main__':
+    logger.info('This is an info message')
+    logger.warning('This is a warning message')
+    logger.error('This is an error message')
+    logger.critical('This is a critical message')
